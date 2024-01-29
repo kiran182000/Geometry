c2c13922c63da48435f96060151ae01f0d4bbe58
kiran <kiran405411@gmail.com>
2024-01-29 13:31:55 +0530

    compare lines

--- src/Linecomparision.java
+++ src/Linecomparision.java
@@ -1,12 +1,24 @@
 public class Linecomparision {
-    public static void main(String args[]){
-        double x1 = 2.0;
-        double y1 = 3.0;
-        double x2 = 5.0;
-        double y2 = 7.0;
-        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        System.out.println("The length of the line segment is: " + length);
+    public static void main(String[] args) {
+        int x1Line1 = 1;
+        int y1Line1 = 8;
+        int x2Line1 = 3;
+        int y2Line1 = 4;
+        int x1Line2 = 2;
+        int y1Line2 = 0;
+        int x2Line2 = 5;
+        int y2Line2 = 6;
+        double lengthLine1 = Math.sqrt(Math.pow((x2Line1 - x1Line1), 2) + Math.pow((y2Line1 - y1Line1), 2));
+        double lengthLine2 = Math.sqrt(Math.pow((x2Line2 - x1Line2), 2) + Math.pow((y2Line2 - y1Line2), 2));
+        Double length1 = lengthLine1;
+        Double length2 = lengthLine2;
+        int comparisonResult = length1.compareTo(length2);
+        if (comparisonResult == 0) {
+            System.out.println("The lines are equal in length.");
+        } else if (comparisonResult > 0) {
+            System.out.println("Line 1 is longer than Line 2.");
+        } else {
+            System.out.println("Line 2 is longer than Line 1.");
+        }
     }
 }
-
-
