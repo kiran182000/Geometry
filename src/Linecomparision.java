c97c6d82b8a9a3ce5cbf4a43f88dbf30d811907b
kiran182000 <157872336+kiran182000@users.noreply.github.com>
2024-01-29 13:10:02 +0530

    welcome message

--- src/Linecomparision.java
+++ src/Linecomparision.java
@@ -1,11 +1,7 @@
 public class Linecomparision {
     public static void main(String args[]){
-        double x1 = 2.0;
-        double y1 = 3.0;
-        double x2 = 5.0;
-        double y2 = 7.0;
-        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        System.out.println("The length of the line segment is: " + length);
+       
+        System.out.println("welcome to line comparision");
     }
 }
 
