519cbccfe7c82f2016ed3b5d00d64398f3545de3
kiran <kiran405411@gmail.com>
2024-01-29 12:51:35 +0530

    Equality of lines

--- src/Linecomparision.java
+++ src/Linecomparision.java
@@ -1,5 +1,19 @@
 public class Linecomparision {
+
     public static void main(String args[]){
-        System.out.println("welcome to line comparision ");
+        int x1Line1 = 0;
+        int y1Line1 = 0;
+        int x2Line1 = 3;
+        int y2Line1 = 4;
+        int x1Line2 = 0;
+        int y1Line2 = 0;
+        int x2Line2 = 3;
+        int y2Line2 = 4;
+        boolean areEqual = (x1Line1 == x1Line2 && y1Line1 == y1Line2 && x2Line1 == x2Line2 && y2Line1 == y2Line2);
+        if (areEqual) {
+            System.out.println("The lines are equal based on their end points.");
+        } else {
+            System.out.println("The lines are not equal based on their end points.");
+        }
     }
 }
